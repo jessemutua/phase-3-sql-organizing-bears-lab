59200412cb5fbc43aa7a3d80bc14abc1f59e6e33
Jesse <jessemutua76@gmail.com>
2023-05-26 09:48:39 +0300

    Completed assignment

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ("Mr Chocolate", 2, "F", "black", "hot", true),
-       ("Rowdy", 3, "M", "brown", "hot", true),
-       ("Tabitha", 4, "F", "white", "hot", true),
-       ("Sergeant Brown", 2, "M", "red", "hot", true),
-       ("Melissa", 1, "F", "blue", "hot", true),
-       ("Grinch", 3, "M", "black", "hot", true),
-       ("Wendy", 5, "F", "white", "hot", true),
-       (NULL, 1, "M", "pink", "hot", true);
+VALUES ("Mr Chocolate", 20, "F", "black", "hot", true),
+       ("Rowdy", 10, "M", "brown", "hot", true),
+       ("Tabitha", 6, "F", "white", "hot", true),
+       ("Sergeant Brown", 19, "M", "red", "hot", true),
+       ("Melissa", 13, "F", "blue", "hot", true),
+       ("Grinch", 2, "M", "black", "hot", true),
+       ("Wendy", 6, "F", "white", "hot", true),
+       (NULL, 20, "M", "pink", "hot", true);
